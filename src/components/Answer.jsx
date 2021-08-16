551435241b490366cb1351f709e6cd0c57f14531
takanori-matsushita <t.matsushita@arcjobz.com>
2021-08-17 01:40:01 +0900

    [fix]send bot when click button

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,17 +1,17 @@
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
+// const useStyles = makeStyles((theme) => ({
+//   root: {
 
-  },
-}));
+//   },
+// }));
 
 const Answer = (props) => {
   // const classes = useStyles();
 
   return (
-    <Button variant="contained">
+    <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
       { props.content }
     </Button>
   )

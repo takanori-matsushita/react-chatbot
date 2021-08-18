2082496c3e3a2b71da773ea7609b87c8fc11aaf0
takanori-matsushita <t.matsushita@arcjobz.com>
2021-08-18 18:12:45 +0900

    [fix]lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,27 @@
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin"
-admin.initializeApp()
-const db = admin.firestore()
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable max-len */
 
 const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
   response.send({
     statusCode,
-    body: JSON.stringify(body)
-  })
-}
+    body: JSON.stringify(body),
+  });
+};
 
 export const addDataset = functions.https.onRequest(async (req: any, res: any) => {
-  if (req.method !== 'POST') {
-    sendResponse(res, 405, { error: 'Invalid Request' })
+  if (req.method !== "POST") {
+    sendResponse(res, 405, {error: "Invalid Request"});
   } else {
-    const dataset = req.body
+    const dataset = req.body;
     for (const key of Object.keys(dataset)) {
-      const data = dataset[key]
-      await db.collection('questions').doc(key).set(data)
+      const data = dataset[key];
+      await db.collection("questions").doc(key).set(data);
     }
-    sendResponse(res, 200, {message: 'Successfully added dataset!'})
+    sendResponse(res, 200, {message: "Successfully added dataset!"});
   }
-})
+});
